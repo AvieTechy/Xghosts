a1710f1acbc6c237bbd51deb76cc6885b5d1c611
Ceo Nhy <wwangrantak99@gmail.com>
2022-11-09 01:40:49 +0700

    update

--- Xghosts/urls.py
+++ Xghosts/urls.py
@@ -8,7 +8,7 @@ from manager.views import (
     index,
 )
 from contract.views import (
-    contract_view,
+    contract_view,  documentations_view, doc1, doc2, doc3, doc4
 )
 from project.views import (
     project_view,
@@ -19,6 +19,12 @@ from members.views import (
 urlpatterns = [
     path('', index, name="index"),
     path('contract/', contract_view, name="contract_view"),
+
+    path('doc1/', doc1, name="doc1"),
+    path('doc2/', doc4, name="doc2"),
+    path('doc3/', doc4, name="doc3"),
+    path('doc4/', doc4, name="doc4"),
+    path('documentations/', documentations_view, name="documentations_view"),
     path('project/', project_view, name="project_view"),
     path('CeoNhy/', CeoNhy, name="CeoNhy"),
     path('HaiLong/', HaiLong, name="HaiLong"),
--- contract/views.py
+++ contract/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def contract_view(request):
 	return render(request, "contract_view.html")
+
+def documentations_view(request):
+	return render(request, "documentations_view.html")
+
+def doc1(request):
+	return render(request, "doc1.html")
+
+def doc2(request):
+	return render(request, "doc2.html")
+
+def doc3(request):
+	return render(request, "doc3.html")
+
+def doc4(request):
+	return render(request, "doc4.html")
